4e1b2053ff45b541f19b4f51bd96b80dc470ea7d
jessebond2 <30394856+jessebond2@users.noreply.github.com>
2023-12-04 23:58:55 -0800

    cleanup and setup day5

--- src/y2023/day5.rs
+++ src/y2023/day5.rs
@@ -116,72 +116,44 @@ mod tests {
 
     #[test]
     fn part_1_test() {
-        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+        let input = "seeds: 79 14 55 13
+
+        seed-to-soil map:
+        50 98 2
+        52 50 48
+        
+        soil-to-fertilizer map:
+        0 15 37
+        37 52 2
+        39 0 15
+        
+        fertilizer-to-water map:
+        49 53 8
+        0 11 42
+        42 0 7
+        57 7 4
+        
+        water-to-light map:
+        88 18 7
+        18 25 70
+        
+        light-to-temperature map:
+        45 77 23
+        81 45 19
+        68 64 13
+        
+        temperature-to-humidity map:
+        0 69 1
+        1 0 69
+        
+        humidity-to-location map:
+        60 56 37
+        56 93 4";
 
         let lines: Vec<String> = input.lines().map(|l| l.trim().to_string()).collect();
         assert_eq!(part_1(&lines), 13);
         let v: Vec<u32> = vec![];
         assert_eq!(v.iter().sum::<u32>(), 0);
-    }
-
-    #[test]
-    fn build_scratch_card() {
-        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
-
-        assert_eq!(
-            ScratchCard::new(input),
-            ScratchCard {
-                winning_numbers: HashSet::from_iter(vec![41, 48, 83, 86, 17]),
-                numbers: HashSet::from_iter(vec![83, 86, 6, 31, 17, 9, 48, 53]),
-                matched: 4,
-                id: 1,
-            }
-        )
-    }
-
-    #[test]
-    fn scratch_card_score() {
-        assert_eq!(
-            ScratchCard::new("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53").score(),
-            8
-        );
-        assert_eq!(
-            ScratchCard::new("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19").score(),
-            2
-        );
-        assert_eq!(
-            ScratchCard::new("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1").score(),
-            2
-        );
-        assert_eq!(
-            ScratchCard::new("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83").score(),
-            1
-        );
-        assert_eq!(
-            ScratchCard::new("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36").score(),
-            0
-        );
-        assert_eq!(
-            ScratchCard::new("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11").score(),
-            0
-        );
-    }
-
-    #[test]
-    fn winnings() {
-        assert_eq!(
-            ScratchCard::new("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53").winnings(10),
-            vec![2, 3, 4, 5]
-        );
-        assert_eq!(
-            ScratchCard::new("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53").winnings(3),
-            vec![2, 3]
-        );
     }
 
     #[test]
@@ -193,6 +165,46 @@ mod tests {
         Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
         Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 
+        let lines: Vec<String> = input.lines().map(|l| l.trim().to_string()).collect();
+        assert_eq!(part_2(&lines), 30)
+    }
+
+    #[test]
+    fn part_2_test2() {
+        let input = "seeds: 79 14 55 13
+
+        seed-to-soil map:
+        50 98 2
+        52 50 48
+        
+        soil-to-fertilizer map:
+        0 15 37
+        37 52 2
+        39 0 15
+        
+        fertilizer-to-water map:
+        49 53 8
+        0 11 42
+        42 0 7
+        57 7 4
+        
+        water-to-light map:
+        88 18 7
+        18 25 70
+        
+        light-to-temperature map:
+        45 77 23
+        81 45 19
+        68 64 13
+        
+        temperature-to-humidity map:
+        0 69 1
+        1 0 69
+        
+        humidity-to-location map:
+        60 56 37
+        56 93 4";
+        
         let lines: Vec<String> = input.lines().map(|l| l.trim().to_string()).collect();
         assert_eq!(part_2(&lines), 30)
     }
