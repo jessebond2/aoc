c62612b2d4f4d9e226651d1423d376a75c6d4d7a
jessebond2 <30394856+jessebond2@users.noreply.github.com>
2023-12-09 14:11:52 -0800

    day2 pt 2

--- src/y2023/day9.rs
+++ src/y2023/day9.rs
@@ -21,12 +21,38 @@ fn next_in_sequence(line: &str) -> i32 {
     next_in_sequence_helper(numbers)
 }
 
+fn previous_in_sequence_helper(numbers: Vec<i32>) -> i32 {
+    let mut deltas: Vec<i32> = vec![];
+
+    if numbers.iter().sum::<i32>() == 0 {
+        return 0;
+    }
+
+    for i in 0..(numbers.len() - 1) {
+        deltas.push(numbers[i + 1] - numbers[i]);
+    }
+
+    numbers[0] - previous_in_sequence_helper(deltas)
+}
+
+fn previous_in_sequence(line: &str) -> i32 {
+    let numbers: Vec<i32> = line
+        .split(" ")
+        .map(|raw| raw.parse::<i32>().unwrap_or(0))
+        .collect();
+
+    previous_in_sequence_helper(numbers)
+}
+
 pub fn part_1(input: &Vec<String>) -> i32 {
     input.iter().map(|line| next_in_sequence(line)).sum()
 }
 
 pub fn part_2(input: &Vec<String>) -> i32 {
-    0
+    input
+        .iter()
+        .map(|line: &String| previous_in_sequence(line))
+        .sum()
 }
 
 #[cfg(test)]
@@ -52,10 +78,18 @@ mod tests {
 
     #[test]
     fn part_2_test() {
-        let input = "Time:      7  15   30
-        Distance:  9  40  200";
+        let input = "0 3 6 9 12 15
+        1 3 6 10 15 21
+        10 13 16 21 30 45";
 
         let lines: Vec<String> = input.lines().map(|l| l.trim().to_string()).collect();
-        assert_eq!(part_2(&lines), 71503);
+        assert_eq!(part_2(&lines), 2);
+    }
+
+    #[test]
+    fn previous_in_sequence_helper_test() {
+        assert_eq!(previous_in_sequence_helper(vec![0, 3, 6, 9, 12, 15]), -3);
+        assert_eq!(previous_in_sequence_helper(vec![1, 3, 6, 10, 15, 21]), 0);
+        assert_eq!(previous_in_sequence_helper(vec![10, 13, 16, 21, 30, 45]), 5);
     }
 }
