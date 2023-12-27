75e519c2ac2c552c5b6673c99c72e093e1342df0
jessebond2 <30394856+jessebond2@users.noreply.github.com>
2023-12-26 17:52:13 -0800

    day 15 pt 1

--- src/y2023/day15.rs
+++ src/y2023/day15.rs
@@ -1,5 +1,11 @@
-pub fn part_1(_input: &str) -> usize {
-    0
+fn hash_value(input: &str) -> u32 {
+    input
+        .chars()
+        .fold(0, |acc, char| ((acc + (char as u32)) * 17) % 256)
+}
+
+pub fn part_1(input: &str) -> u32 {
+    input.split(',').map(hash_value).sum()
 }
 
 pub fn part_2(_input: &str) -> usize {
@@ -12,9 +18,15 @@ mod tests {
 
     #[test]
     fn part_1_test() {
-        let input = "";
+        let input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
 
-        assert_eq!(part_1(input), 288);
+        assert_eq!(part_1(input), 1320);
+    }
+
+    #[test]
+    fn hash_value_test() {
+        assert_eq!(hash_value("HASH"), 52);
+        assert_eq!(hash_value("rn=1"), 30);
     }
 
     #[test]
