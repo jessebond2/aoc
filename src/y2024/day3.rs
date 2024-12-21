634ff86f1363d55e24afa7f06593bbb039157bb9
jessebond2 <30394856+jessebond2@users.noreply.github.com>
2024-12-20 19:06:57 -0800

    day 3

--- src/y2024/day3.rs
+++ src/y2024/day3.rs
@@ -1,9 +1,76 @@
-pub fn part_1(_input: &str) -> i32 {
-    0
+use regex::Regex;
+
+#[derive(PartialEq, Clone, Copy)]
+enum Op {
+    Do,
+    Dont,
+    Mul,
 }
 
-pub fn part_2(_input: &str) -> i32 {
-    0
+#[derive(Debug)]
+struct State {
+    enabled: bool,
+    count: i32,
+}
+
+pub fn part_2(input: &str) -> i32 {
+    let regex = Regex::new(r"(mul)\((\d{1,3}),(\d{1,3})\)|(do)\(\)()()|(don't)\(\)()()").unwrap();
+    let all: Vec<(Op, i32, i32)> = regex
+        .captures_iter(input)
+        .map(|caps| {
+            let (_, [op, a, b]) = caps.extract();
+            match op {
+                "mul" => (
+                    Op::Mul,
+                    a.parse::<i32>().unwrap(),
+                    b.parse::<i32>().unwrap(),
+                ),
+                "do" => (Op::Do, 0, 0),
+                _ => (Op::Dont, 0, 0),
+            }
+        })
+        .collect();
+
+    all.into_iter()
+        .fold(
+            State {
+                enabled: true,
+                count: 0,
+            },
+            |acc, (op, a, b)| match op {
+                Op::Do => State {
+                    enabled: true,
+                    count: acc.count,
+                },
+                Op::Dont => State {
+                    enabled: false,
+                    count: acc.count,
+                },
+                Op::Mul => {
+                    if acc.enabled {
+                        State {
+                            enabled: true,
+                            count: acc.count + a * b,
+                        }
+                    } else {
+                        acc
+                    }
+                }
+            },
+        )
+        .count
+}
+
+pub fn part_1(input: &str) -> i32 {
+    let regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let all: Vec<(i32, i32)> = regex
+        .captures_iter(input)
+        .map(|caps| {
+            let (_, [a, b]) = caps.extract();
+            (a.parse::<i32>().unwrap(), b.parse::<i32>().unwrap())
+        })
+        .collect();
+    all.into_iter().fold(0, |acc, (a, b)| acc + a * b)
 }
 
 #[cfg(test)]
@@ -12,25 +79,15 @@ mod tests {
 
     #[test]
     fn part_1_test() {
-        let input = "3   4
-4   3
-2   5
-1   3
-3   9
-3   3";
+        let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
-        assert_eq!(part_1(input), 0);
+        assert_eq!(part_1(input), 161);
     }
 
     #[test]
     fn part_2_test() {
-        let input = "3   4
-4   3
-2   5
-1   3
-3   9
-3   3";
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
-        assert_eq!(part_2(input), 0);
+        assert_eq!(part_2(input), 48);
     }
 }
